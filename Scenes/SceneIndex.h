32b224981b88d9b9d6b71a4c167350c380cb06bf
Cem Bektasoglu <cembektasoglu7@gmail.com>
2024-12-15 03:25:39 +0100

    Code clean

--- Scenes/SceneIndex.h
+++ Scenes/SceneIndex.h
@@ -2,9 +2,6 @@
 #include <map>
 
 #include "Scene1.h"
-#include "Scene2.h"
-#include "Scene3.h"
-#include "Scene4.h"
 
 
 using SceneCreator = std::function<std::unique_ptr<Scene>()>;
@@ -18,7 +15,4 @@ SceneCreator creator()
 
 std::map<std::string, SceneCreator> scenesCreators = {
     {"1 Single Step", creator<Scene1>()},
-    {"2 Simulation", creator<Scene2>()},
-    {"3 Collision", creator<Scene3>()},
-    {"4 Complex Scene", creator<Scene4>()},
 };
